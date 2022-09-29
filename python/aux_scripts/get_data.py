dac7c3f53d444725887b0bd9dc1b4707d7f5efd8
Uglykoyote <uglykoyote@yandex.ru>
2022-09-29 22:31:29 +0400

    Add logger level (#2)
    * Added support external logging level config

--- python/aux_scripts/get_data.py
+++ python/aux_scripts/get_data.py
@@ -9,11 +9,26 @@ import random
 import logging
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s :: %(levelname)s :: %(message)s'
+)
+parent_logger = logging.getLogger("parent")
+
+
+def configure_logger(logLevel):
+    if logLevel == 'INFO':
+        parent_logger.setLevel(logging.INFO)
+    elif logLevel == 'DEBUG':
+        parent_logger.setLevel(logging.INFO)
+    elif logLevel == 'ERROR':
+        parent_logger.setLevel(logging.ERROR)
+    # print(f'{logLevelSeverity}'.format(logLevelSeverity=logLevelSeverity))
+    # logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(message)s')
 
 
 def curr_time():
-    logging.debug("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.debug("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     dt = datetime.now().strftime("%H:%M:%S.%f")[:-4]
     return dt
 
@@ -31,7 +46,7 @@ def generate_bulk(count, locale, json_file):
     can be reached with URL: https://faker.readthedocs.io/en/master/locales.html
     If needed locale is working without error, you should update list supportedLocale to be added locale in supported
     """
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     person = {}
     fake = Faker(locale)
     values_type = get_header_fields_name('fields', json_file)
@@ -41,12 +56,12 @@ def generate_bulk(count, locale, json_file):
             for k, v in values_type.items():
                 person[i][k] = eval(v)
     except Exception as e:
-        logging.error("Something wrong in function {message}. Error message {error}".format(message=sys._getframe(0).f_code.co_name, error=e))
+        parent_logger.error("Something wrong in function {message}. Error message {error}".format(message=sys._getframe(0).f_code.co_name, error=e))
     return person
 
 
 def get_cvs_headers_name(json_file):
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     res = []
     with open(json_file) as json_file:
         data = json.load(json_file)
@@ -58,7 +73,7 @@ def get_cvs_headers_name(json_file):
 
 
 def save_data_to_csv(*args):
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     headers = args[0]
     records = args[1]
     data = []
@@ -74,7 +89,7 @@ def save_data_to_csv(*args):
 
 
 def read_headers_json(headers_json_file_name, mode):
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     if mode == 'headers':
         res = []
     else:
@@ -87,7 +102,7 @@ def read_headers_json(headers_json_file_name, mode):
                 try:
                     res.append(data['headers'][keyIdx])
                 except Exception as e:
-                    logging.error("Json file is malformed. Error is: {message}".format(message=e))
+                    parent_logger.error("Json file is malformed. Error is: {message}".format(message=e))
         else:
             for count, item in enumerate(data['fields'], start=1):
                 keyIdx = f'key{count}'.format(count)
@@ -98,29 +113,29 @@ def read_headers_json(headers_json_file_name, mode):
 
 
 def get_header_fields_name(mode, json_file):
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     if mode == 'headers':
         try:
             res = read_headers_json(json_file, mode)
         except Exception as e:
-            logging.error("Unable to execute get_header_fields_name. Error is: {message}".format(message=e))
+            parent_logger.error("Unable to execute get_header_fields_name. Error is: {message}".format(message=e))
             res = ['uuid', 'fio', 'phone', 'age', 'address', 'email']
     else:
         try:
             res = read_headers_json(json_file, mode)
         except Exception as e:
-            logging.error("Unable to execute get_header_fields_name. Error is: {message}".format(message=e))
+            parent_logger.error("Unable to execute get_header_fields_name. Error is: {message}".format(message=e))
     return res
 
 
 def read_env():
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     supportedLocale = ['ru_RU', 'pl_PL', 'en_US', 'en_GB', 'fr_FR', 'ja_JP']
     if os.getenv('LOCALE') in supportedLocale:
         LOCALE = os.getenv('LOCALE')
     else:
         LOCALE = 'ru_RU'
-    logging.debug("Env variable LOCALE set to: {message}".format(message=LOCALE))
+    parent_logger.debug("Env variable LOCALE set to: {message}".format(message=LOCALE))
     if os.getenv('PERSON_COUNT'):
         try:
             PERSON_COUNT = int(os.getenv('PERSON_COUNT'))
@@ -128,22 +143,22 @@ def read_env():
             PERSON_COUNT = 10
     else:
         PERSON_COUNT = 10
-    logging.debug("Env variable PERSON_COUNT set to: {message}".format(message=PERSON_COUNT))
+    parent_logger.debug("Env variable PERSON_COUNT set to: {message}".format(message=PERSON_COUNT))
     if os.getenv('OUTPUT_FILE_NAME'):
         OUTPUT_FILE_NAME = os.getenv('OUTPUT_FILE_NAME')
     else:
         OUTPUT_FILE_NAME = 'default.csv'
-    logging.debug("Env variable OUTPUT_FILE_NAME set to: {message}".format(message=OUTPUT_FILE_NAME))
+    parent_logger.debug("Env variable OUTPUT_FILE_NAME set to: {message}".format(message=OUTPUT_FILE_NAME))
     if os.getenv('JSON_TEMPLATE_FILE'):
         JSON_TEMPLATE_FILE = os.getenv('JSON_TEMPLATE_FILE')
     else:
         JSON_TEMPLATE_FILE = 'headers.json'
-    logging.debug("Env variable JSON_TEMPLATE_FILE set to: {message}".format(message=JSON_TEMPLATE_FILE))
+    parent_logger.debug("Env variable JSON_TEMPLATE_FILE set to: {message}".format(message=JSON_TEMPLATE_FILE))
     return (PERSON_COUNT, OUTPUT_FILE_NAME, JSON_TEMPLATE_FILE, LOCALE)
 
 
 def actions(PERSON_COUNT, OUTPUT_FILE_NAME, JSON_TEMPLATE_FILE, LOCALE):
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     json_file = JSON_TEMPLATE_FILE
     persons = generate_bulk(PERSON_COUNT, LOCALE, json_file)
     headers = get_cvs_headers_name(json_file)
@@ -151,14 +166,19 @@ def actions(PERSON_COUNT, OUTPUT_FILE_NAME, JSON_TEMPLATE_FILE, LOCALE):
 
 
 def main():
-    logging.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    if os.getenv('LOGGER_LEVEL_SEVERITY'):
+        logLevel = os.getenv('LOGGER_LEVEL_SEVERITY')
+    else:
+        logLevel = 'INFO'
+    configure_logger(logLevel)
     PERSON_COUNT, OUTPUT_FILE_NAME, JSON_TEMPLATE_FILE, LOCALE = read_env()
+    parent_logger.info("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     try:
         actions(PERSON_COUNT, OUTPUT_FILE_NAME, JSON_TEMPLATE_FILE, LOCALE)
     except Exception as e:
-        logging.error("Unable to execute Actions. Error: {message}".format(message=e))
+        parent_logger.error("Unable to execute Actions. Error: {message}".format(message=e))
 
 
 if __name__ == '__main__':
-    logging.debug("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
+    parent_logger.debug("Called function {message}".format(message=sys._getframe(0).f_code.co_name))
     main()
